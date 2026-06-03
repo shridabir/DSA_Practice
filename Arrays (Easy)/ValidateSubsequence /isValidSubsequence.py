16c62231b8f43a469477f80a8d58ba584d3c5d0d
Shrirang Dabir <60786610+shridabir@users.noreply.github.com>
2026-06-03 13:34:42 +0530

    Implement isValidSubsequence function

---
+++ Arrays (Easy)/ValidateSubsequence /isValidSubsequence.py
@@ -0,0 +1,10 @@
+def isValidSubsequence(array, sequence):
+    # Write your code here.
+    arrIdx = 0
+    seqIdx = 0
+    valSubsequence = True
+    while arrIdx < len(array) and seqIdx < len(sequence):
+        if array[arrIdx] == sequence[seqIdx]:
+            seqIdx += 1
+        arrIdx += 1
+    return seqIdx == len(sequence)
