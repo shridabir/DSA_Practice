af29cd06aca7942b01460b05a1ffcaff60b1c720
Shrirang Dabir <60786610+shridabir@users.noreply.github.com>
2026-06-21 21:17:56 -0400

    Enhance threeNumberSum with duplicate handling
    Refactor threeNumberSum function to handle duplicates and improve clarity.

--- Arrays (Easy)/Three Number Sum /threeNumberSum.py
+++ Arrays (Easy)/Three Number Sum /threeNumberSum.py
@@ -1,21 +1,34 @@
 #two pointer approach O(n^2) time | O(n) space to store triplets
 def threeNumberSum(array, targetSum):
     # Write your code here.
-    #sort the array: nlogn
     array.sort()
+    n = len(array)
     triplets = []
 
-    for i in range (len(array) - 2): #n - 2 because last number we will traverse in array will be 3rd last as we will sum up atleast 2 other values with current number in array
-        left = i + 1 #right next to current num
-        right = len(array) - 1 #last element in the array
-        while (left < right):
-            currSum = array[i] + array[left] + array[right]
-            if currSum == targetSum:
+    for i in range (n - 2):
+        #skip duplicates for the first number
+        if i > 0 and array[i] == array[i - 1]:
+            continue
+
+        left = i + 1
+        right = n - 1
+        while left < right:
+            summ = array[i] + array[left] + array[right]
+            if summ == targetSum:
                 triplets.append([array[i], array[left], array[right]])
+                # Check 1: Skip duplicates for the left pointer
+                while left < right and array[left] == array[left + 1]:
+                    left += 1
+                # Check 2: Skip duplicates for the right pointer
+                while left < right and array[right] == array[right - 1]:
+                    right -= 1
+
                 left += 1
                 right -= 1
-            elif currSum < targetSum:
+
+            elif summ < targetSum:
                 left += 1
             else:
                 right -= 1
+
     return triplets
