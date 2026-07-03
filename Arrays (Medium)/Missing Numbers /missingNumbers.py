55d5f0df9efdd1068d8ec77896b49bf6970d71a0
Shrirang Dabir <60786610+shridabir@users.noreply.github.com>
2026-07-03 11:59:48 -0400

    Add function to find missing numbers in a list
    Implement logic to find missing numbers using two methods: set inclusion and sum averages.

--- Arrays (Medium)/Missing Numbers /missingNumbers.py
+++ Arrays (Medium)/Missing Numbers /missingNumbers.py
@@ -1,5 +1,6 @@
 def missingNumbers(nums):
     # Write your code here.
+    '''
     #M-1 using set: TC = O(n) | SC = O(n)
     includedNumbers = set(nums)
     solutions = []
@@ -8,4 +9,31 @@ def missingNumbers(nums):
         if num not in includedNumbers:
             solutions.append(num)
     return solutions
+    return []
+    '''
+
+    #M-2: using sum and averages logic
+
+    #find total of expected range which is from 1 to len(nums) + 2
+    total = sum(range(1, len(nums) + 3))
+
+    #subtract total of nums from this
+    for num in nums:
+        total -= num
+    
+    averageMissingValue = total // 2 #average of missing values
+    foundFirstHalf = 0
+    foundSecondHalf = 0
+
+    #traverse through nums array if value is <= avg add it in firstHalf or else add it in secondHalf
+    for num in nums:
+        if num <= averageMissingValue:
+            foundFirstHalf += num
+        else:
+            foundSecondHalf += num
+
+    expectedFirstHalf = sum(range(1, averageMissingValue + 1))
+    expectedSecondHalf = sum(range(averageMissingValue + 1, len(nums) + 3))
+
+    return [expectedFirstHalf - foundFirstHalf, expectedSecondHalf - foundSecondHalf]
     return []
